aa39b16d6e37da6332caf2c42752e98542ddb343
Vinicius Cosmi <vinicius.cosmi@gmail.com>
2022-08-21 19:30:47 -0300

    change way to execution and parameters

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 API_URL = 'http://localhost:3000/usuarios'
+VALUES = [None, '', 123, True]
 
 with open('payload/body.json', 'r') as file:
   body = json.load(file)
@@ -21,49 +22,15 @@ def save_log(value):
     f.write('\n\n')
     f.close()
 
-def change_values_from_body_to_none(body):
+def change_values_from_body(body, value):
     for k, v in body.items():
         if isinstance(v, dict):
-            change_values_from_body_to_none(v)
+            change_values_from_body(v, value)
         else:
             initial_value = body[k]
             body[k] = None
             send_request()
             body[k] = initial_value
 
-def change_values_from_body_to_empty(body):
-    for k, v in body.items():
-        if isinstance(v, dict):
-            change_values_from_body_to_empty(v)
-        else:
-            initial_value = body[k]
-            body[k] = ''
-            send_request()
-            body[k] = initial_value
-
-def change_values_from_body_to_int(body):
-    for k, v in body.items():
-        if isinstance(v, dict):
-            change_values_from_body_to_int(v)
-        else:
-            initial_value = body[k]
-            body[k] = 1
-            send_request()
-            body[k] = initial_value
-
-def change_values_from_body_to_bool(body):
-    for k, v in body.items():
-        if isinstance(v, dict):
-            change_values_from_body_to_bool(v)
-        else:
-            initial_value = body[k]
-            body[k] = True
-            send_request()
-            body[k] = initial_value
-
-
-
-change_values_from_body_to_none(body)
-change_values_from_body_to_empty(body)
-change_values_from_body_to_int(body)
-change_values_from_body_to_bool(body)
+for i in VALUES:
+    change_values_from_body(body, i)
